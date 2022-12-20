bb0629fae3d5f2d9d5f8db2770ca3b77ce8678c6
Perkova Anastasiya <44846835+anastasiyaperk@users.noreply.github.com>
2022-12-20 20:33:04 +0500

    Add multi connections logic for memcache

--- memc_connection.py
+++ memc_connection.py
@@ -1,6 +1,7 @@
 import functools
+import logging
 import time
-from typing import Any
+from typing import Any, Dict
 
 import pymemcache
 
@@ -9,6 +10,7 @@ def retry(exception=Exception, retries=3, backoff_in_seconds=1):
     """
     Decorator for retrying
     """
+
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -16,7 +18,7 @@ def retry(exception=Exception, retries=3, backoff_in_seconds=1):
                 try:
                     return func(*args, **kwargs)
                 except exception as e:
-                    print(f'Retrying {func.__name__}: {i}/{retries}')
+                    logging.warning(f'Retrying {func.__name__}: {i}/{retries}')
                     time.sleep(backoff_in_seconds * 2 ** i)
                     if i == retries:
                         raise e
@@ -27,13 +29,14 @@ def retry(exception=Exception, retries=3, backoff_in_seconds=1):
 
 
 class MemcacheClient:
-    def __init__(self, addr: str, port: int, timeout: float = 5.):
-        self.__client = pymemcache.client.base.Client((addr, port), timeout=timeout)
+    def __init__(self, addr: str, timeout: float = 5.):
+        self.__client = pymemcache.client.base.Client(addr, timeout=timeout)
 
     @retry(ConnectionRefusedError)
     def cache_get(self, key) -> Any:
         """
         Get value with memcache client
+
         :param key: key
         :return: value in mtmcache by key
         """
@@ -43,6 +46,7 @@ class MemcacheClient:
     def cache_set(self, key, value, expire_time: int = 60):
         """
         Set value with memcache client
+
         :param key: key
         :param value: value to set on key
         :param expire_time: number of seconds until the item is expired from the cache
@@ -50,3 +54,19 @@ class MemcacheClient:
         """
         return self.__client.set(key, value, expire_time)
 
+
+def get_memc_client(addr: str, memcache_connections: Dict[str, MemcacheClient]) -> MemcacheClient:
+    """
+    Get Memcache client, or create if it is new connection
+
+    :param memcache_connections: dict for storing memcache connection
+    :param addr: memcache server address
+    :return: Memcache client object
+    """
+    if addr not in memcache_connections:
+        logging.info(f"New connection with {addr}")
+        memcache_connections[addr] = MemcacheClient(addr=addr)
+    else:
+        logging.info(f"Connecting with {addr}")
+
+    return memcache_connections[addr]
