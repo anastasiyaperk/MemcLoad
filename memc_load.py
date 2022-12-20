021f66e8ffc75ff1ec9a8fe7e9a4da92270412cc
Perkova Anastasiya <44846835+anastasiyaperk@users.noreply.github.com>
2022-12-20 20:31:08 +0500

    Update base memc_load to python 3.8

--- memc_load.py
+++ memc_load.py
@@ -5,17 +5,22 @@ import gzip
 import sys
 import glob
 import logging
-import collections
+from collections import namedtuple
 from optparse import OptionParser
+from typing import Union
+
 # brew install protobuf
 # protoc  --python_out=. ./appsinstalled.proto
 # pip install protobuf
 import appsinstalled_pb2
+from memc_connection import MemcacheClient
+
 # pip install python-memcached
-import memcache
+# import memcache
+# from pymemcache.client import base
 
 NORMAL_ERR_RATE = 0.01
-AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
+AppsInstalled = namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 
 
 def dot_rename(path):
@@ -24,43 +29,48 @@ def dot_rename(path):
     os.rename(path, os.path.join(head, "." + fn))
 
 
-def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
+def insert_appsinstalled(memc_addr: str, appsinstalled: AppsInstalled, dry_run=False):
     ua = appsinstalled_pb2.UserApps()
     ua.lat = appsinstalled.lat
     ua.lon = appsinstalled.lon
     key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
     ua.apps.extend(appsinstalled.apps)
     packed = ua.SerializeToString()
-    # @TODO persistent connection
-    # @TODO retry and timeouts!
     try:
         if dry_run:
             logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
         else:
-            memc = memcache.Client([memc_addr])
-            memc.set(key, packed)
+            addr, port = memc_addr.split(":")
+            memc_client = MemcacheClient(addr=addr)
+            memc_client.cache_set(key, packed)
+
     except Exception as e:
         logging.exception("Cannot write to memc %s: %s" % (memc_addr, e))
         return False
+
     return True
 
 
-def parse_appsinstalled(line):
+def parse_appsinstalled(line:str) -> Union[AppsInstalled, None]:
     line_parts = line.strip().split("\t")
     if len(line_parts) < 5:
         return
+
     dev_type, dev_id, lat, lon, raw_apps = line_parts
     if not dev_type or not dev_id:
         return
+
     try:
         apps = [int(a.strip()) for a in raw_apps.split(",")]
     except ValueError:
         apps = [int(a.strip()) for a in raw_apps.split(",") if a.isidigit()]
         logging.info("Not all user apps are digits: `%s`" % line)
+
     try:
         lat, lon = float(lat), float(lon)
     except ValueError:
         logging.info("Invalid geo coords: `%s`" % line)
+
     return AppsInstalled(dev_type, dev_id, lat, lon, apps)
 
 
@@ -70,7 +80,7 @@ def main(options):
         "gaid": options.gaid,
         "adid": options.adid,
         "dvid": options.dvid,
-    }
+        }
     for fn in glob.iglob(options.pattern):
         processed = errors = 0
         logging.info('Processing %s' % fn)
@@ -79,20 +89,24 @@ def main(options):
             line = line.strip()
             if not line:
                 continue
+
             appsinstalled = parse_appsinstalled(line)
             if not appsinstalled:
                 errors += 1
                 continue
+
             memc_addr = device_memc.get(appsinstalled.dev_type)
             if not memc_addr:
                 errors += 1
                 logging.error("Unknow device type: %s" % appsinstalled.dev_type)
                 continue
+
             ok = insert_appsinstalled(memc_addr, appsinstalled, options.dry)
             if ok:
                 processed += 1
             else:
                 errors += 1
+
         if not processed:
             fd.close()
             dot_rename(fn)
@@ -103,6 +117,7 @@ def main(options):
             logging.info("Acceptable error rate (%s). Successfull load" % err_rate)
         else:
             logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
+
         fd.close()
         dot_rename(fn)
 
@@ -128,14 +143,17 @@ if __name__ == '__main__':
     op.add_option("-t", "--test", action="store_true", default=False)
     op.add_option("-l", "--log", action="store", default=None)
     op.add_option("--dry", action="store_true", default=False)
-    op.add_option("--pattern", action="store", default="/data/appsinstalled/*.tsv.gz")
+    op.add_option("--pattern", action="store", default="*.tsv.gz")
     op.add_option("--idfa", action="store", default="127.0.0.1:33013")
     op.add_option("--gaid", action="store", default="127.0.0.1:33014")
     op.add_option("--adid", action="store", default="127.0.0.1:33015")
     op.add_option("--dvid", action="store", default="127.0.0.1:33016")
     (opts, args) = op.parse_args()
-    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+
+    logging.basicConfig(filename=opts.log,
+                        level=logging.INFO if not opts.dry else logging.DEBUG,
+                        format='[%(asctime)s] %(levelname).1s %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S')
     if opts.test:
         prototest()
         sys.exit(0)
